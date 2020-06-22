67f2d4c280f302e2397b0fe629949afe36a27161
Ryan Holmes <rholmes@emagine.com>
2020-06-21 22:20:46 -0400

    Add base font styles and update some core block styles.

--- build/functions/enqueue.php
+++ build/functions/enqueue.php
@@ -15,6 +15,7 @@
  * @link  https://developer.wordpress.org/reference/functions/wp_enqueue_style/
  */
 function dc2dc_enqueue_styles() {
-	wp_enqueue_style( 'dc2dc-css', get_stylesheet_directory_uri() . '/css/main.css', array( 'go-style' ), '1.0.0' );
+	wp_enqueue_style( 'dc2dc-google-fonts-css', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&family=Open+Sans&display=swap', array(), '1.0.0' );
+	wp_enqueue_style( 'dc2dc-css', get_stylesheet_directory_uri() . '/css/main.css', array( 'go-style', 'dc2dc-google-fonts-css' ), '1.0.0' );
 }
-add_action( 'wp_enqueue_scripts', 'dc2dc_enqueue_styles' );
+add_action( 'enqueue_block_assets', 'dc2dc_enqueue_styles' );
