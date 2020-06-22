686ead914f561a16df58dbef61fb6649ebef0109
Ryan Holmes <rholmes@emagine.com>
2020-06-22 19:35:27 -0400

    Update typography and various block styles.

--- build/functions/enqueue.php
+++ build/functions/enqueue.php
@@ -15,7 +15,25 @@
  * @link  https://developer.wordpress.org/reference/functions/wp_enqueue_style/
  */
 function dc2dc_enqueue_styles() {
-	wp_enqueue_style( 'dc2dc-google-fonts-css', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&family=Open+Sans&display=swap', array(), '1.0.0' );
-	wp_enqueue_style( 'dc2dc-css', get_stylesheet_directory_uri() . '/css/main.css', array( 'go-style', 'dc2dc-google-fonts-css' ), '1.0.0' );
+	wp_enqueue_style( 'dc2dc-css', get_stylesheet_directory_uri() . '/css/main.css', array( 'go-style' ), '1.0.0' );
 }
 add_action( 'enqueue_block_assets', 'dc2dc_enqueue_styles' );
+
+/**
+ * Update font family for modern design.
+ *
+ * @param array $styles Array of Go theme styles.
+ * @return array
+ */
+function dc2dc_go_design_styles( $styles ) {
+	$styles['modern']['fonts'] = array(
+		'Roboto' => array(
+			'300',
+			'400',
+			'700',
+			'900',
+		),
+	);
+	return $styles;
+}
+add_filter( 'go_design_styles', 'dc2dc_go_design_styles' );
