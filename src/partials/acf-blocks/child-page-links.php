8f5e55b9932265a1d6253e2cf6c7f81cb11f8329
Ryan Holmes <rholmes@emagine.com>
2020-07-01 23:54:45 -0400

    Add option to include the parent page in the child page links block.

--- src/partials/acf-blocks/child-page-links.php
+++ src/partials/acf-blocks/child-page-links.php
@@ -5,33 +5,40 @@
  * @package dc2dc
  */
 
-$parent_page = get_field( 'parent_page' );
-if ( $parent_page ) :
+$data = get_fields();
+if ( ! empty( $data['parent_page'] ) ) :
 	$args = array(
 		'post_type'      => 'page',
-		'post_parent'    => $parent_page,
+		'post_parent'    => $data['parent_page'],
 		'posts_per_page' => 99,
 	);
 
-	if ( get_the_ID() !== $parent_page ) {
+	if ( get_the_ID() !== $data['parent_page'] ) {
 		$args['post__not_in'] = array( get_the_ID() );
 	}
 
 	$child_pages = get_posts( $args );
+
 	if ( $child_pages ) :
+		$parent_page = false;
+		if ( ! empty( $data['include_parent_page'] ) ) {
+			$parent_page = get_post( $data['parent_page'] );
+			if ( $parent_page ) {
+				array_unshift( $child_pages, $parent_page );
+			}
+		}
 		?>
 		<div class="dc2dc-child-page-links__inner alignwide">
-			<?php
-			$block_title = get_field( 'title' );
-			if ( $block_title ) :
-				?>
-				<h2><?php echo wp_kses_post( $block_title ); ?></h2>
+			<?php if ( ! empty( $data['title'] ) ) : ?>
+				<h2><?php echo wp_kses_post( $data['title'] ); ?></h2>
 			<?php endif; ?>
 			<ul>
 				<?php foreach ( $child_pages as $child_page ) : ?>
 					<li>
 						<div class="wp-block-button">
-							<a class="wp-block-button__link" <?php dc2dc_href_attrs( get_permalink( $child_page ) ); ?>><?php echo wp_kses_post( get_the_title( $child_page ) ); ?></a>
+							<a class="wp-block-button__link" <?php dc2dc_href_attrs( get_permalink( $child_page ) ); ?>>
+								<?php echo wp_kses_post( $child_page === $parent_page ? __( 'Overview', 'dc2dc' ) : get_the_title( $child_page ) ); ?>
+							</a>
 						</div>
 					</li>
 				<?php endforeach; ?>
